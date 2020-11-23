7a751cf03d95926a39b29b75395b3c44f890ee14
daria-yermakova <daria.yerm0710@gmail.com>
2020-11-23 20:43:18 +0200

    make methods, which makes clients and rooms for producer

--- src/main/java/com/demo/Application.java
+++ src/main/java/com/demo/Application.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.Locale;
 
 @SpringBootApplication
+@EnableScheduling
 @Configuration
 @PropertySource(value = "classpath:application.properties")
 public class Application {
--- src/main/java/com/demo/services/RoomService.java
+++ src/main/java/com/demo/services/RoomService.java
@@ -2,19 +2,42 @@ package com.demo.services;
 
 import com.demo.translators.RoomTranslator;
 import com.demo.types.Room;
+import lombok.extern.log4j.Log4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.scheduling.annotation.Scheduled;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
+
+@Log4j
 @Component
 public class RoomService {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private RoomTypeService roomTypeService;
+
+    public static final List<String> DESCRIPTIONS = Arrays.asList("Rich room", "Cool room", "Beautiful room");
+    public static final List<Integer> NUMBERS = Arrays.asList(7097911, 7095655, 7098585);
+    public Logger logger = LoggerFactory.getLogger(RoomService.class);
+
+    @Scheduled(initialDelay = 1000, fixedDelay = 8000)
+    public void makeRoom() {
+        Random random = new Random();
+        int roomNumber = getAllRooms().get(getAllRooms().size()-1).getRoomNumber() + random.nextInt(25);
+        Room room = add(roomNumber,
+                NUMBERS.get(random.nextInt(NUMBERS.size())),
+                roomTypeService.getAllRoomTypes().get(random.nextInt(roomTypeService.getAllRoomTypes().size())).getId(),
+                random.nextInt(25),
+                DESCRIPTIONS.get(random.nextInt(DESCRIPTIONS.size())));
+        logger.info("We create a room:" + room.toString());
+    }
 
     public Room add(int roomNumber, int phoneNumber, int roomType, int floor, String description) {
         jdbcTemplate.update("Insert Into Room(room_number, phone_number, room_type, floor, description) Values(?,?,?,?,?)",
